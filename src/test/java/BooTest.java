89349aa013c8c7784d280867170bb81843b96a93
Natalia Khrol <natalia_khrol@epam.com>
2024-06-18 11:48:37 +0300

    Initial commit

---
+++ src/test/java/BooTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+public class BooTest {
+
+    //ЕСЛИ ЧИСЛО КРАТНО 3, ВОЗВРАЩАТЬ "T"
+    //ЕСЛИ ЧИСЛО КРАТНО 5, ВОЗВРАЩАТЬ "M"
+    //ЕСЛИ ЧИСЛО КРАТНО 3 B КРАТНО 5 , ВОЗВРАЩАТЬ "TMS"
+    //"FAIL"
+
+   /* public String tms(int number) {
+
+    }*/
+
+    @Test
+    public void zipCode(){
+
+    }
+
+
+}
